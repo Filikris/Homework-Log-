f778ccab99c94291c60c05667befde5e460d96b5
Kristina <kristina.filippova@dxc.com>
2022-01-12 15:54:08 +0100

    add buttons, text fields and check button

--- lux.task.jface/src/main/java/lux/task/jface/Main.java
+++ lux.task.jface/src/main/java/lux/task/jface/Main.java
@@ -8,10 +8,16 @@ import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 
 
 public class Main extends ApplicationWindow {
@@ -23,7 +29,10 @@ public class Main extends ApplicationWindow {
 
 	  protected Control createContents(Composite parent) {
 		  getShell().setText("JFace homework log");
-		  final TableViewer viewer = new TableViewer(parent,
+		  SashForm mainPane = new SashForm( parent, SWT.HORIZONTAL );
+		  Composite tableComposite = new Composite(mainPane, SWT.NONE);
+		  tableComposite.setLayout(new GridLayout());
+		  final TableViewer viewer = new TableViewer(tableComposite,
 				  SWT.BORDER | SWT.FULL_SELECTION);
 		  TableLayout layout = new TableLayout();
 		  layout.addColumnData(new ColumnWeightData(33, true));
@@ -40,6 +49,47 @@ public class Main extends ApplicationWindow {
 		  column2.setText("Group");
 		  TableColumn column3 = new TableColumn(viewer.getTable(), SWT.CENTER);
 		  column3.setText("SWT done");
+		  
+		  Composite studentComposite = new Composite(mainPane, SWT.NONE);
+		  studentComposite.setLayout(new GridLayout(4, false));
+		  studentComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		  Label nameLabel = new Label(studentComposite, SWT.LEFT);
+		  nameLabel.setText("Name");
+		  nameLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		  Text nameText = new Text(studentComposite, SWT.SINGLE | SWT.BORDER);
+		  GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		  gridData.horizontalSpan = 3;
+		  nameText.setLayoutData(gridData);
+		  
+		  Label groupLabel = new Label(studentComposite, SWT.LEFT);
+		  groupLabel.setText("Group");
+		  groupLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		  Text groupText = new Text(studentComposite, SWT.SINGLE | SWT.BORDER);
+		  gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		  gridData.horizontalSpan = 3;
+		  groupText.setLayoutData(gridData);
+		  
+		  Button taskDone = new Button(studentComposite, SWT.CHECK);
+		  taskDone.setText("SWT task done");
+		  gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		  gridData.horizontalSpan = 4;
+		  taskDone.setLayoutData(gridData);
+		  
+		  Button addNew = new Button(studentComposite,SWT.NONE);
+		  addNew.setText("New");
+		  addNew.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
+		  
+		  Button save = new Button(studentComposite,SWT.NONE);
+		  save.setText("Save");
+		  save.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
+		  
+		  Button delete = new Button(studentComposite,SWT.NONE);
+		  delete.setText("Delete");
+		  delete.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
+		  
+		  Button cancel = new Button(studentComposite,SWT.NONE);
+		  cancel.setText("Cancel");
+		  cancel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
 
 		  parent.pack();
 	    
