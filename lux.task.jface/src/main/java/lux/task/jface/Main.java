ba7df1a543e81ddd3b4b8c21f450eb48b72f0caf
Kristina <kristina.filippova@dxc.com>
2022-01-13 17:13:31 +0100

    add Student class and StudentTable class, moved button Delete to table

--- lux.task.jface/src/main/java/lux/task/jface/Main.java
+++ lux.task.jface/src/main/java/lux/task/jface/Main.java
@@ -1,11 +1,11 @@
 package lux.task.jface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.TableLayout;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
@@ -16,49 +16,50 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
-
 
 public class Main extends ApplicationWindow {
 
 	  public Main() {	    
 		  super(null);
+
 		  createMenu();
 	  }
 
 	  protected Control createContents(Composite parent) {
 		  getShell().setText("JFace homework log");
-		  SashForm mainPane = new SashForm( parent, SWT.HORIZONTAL );
+
+		  SashForm mainPane = new SashForm(parent, SWT.HORIZONTAL);
 		  Composite tableComposite = new Composite(mainPane, SWT.NONE);
 		  tableComposite.setLayout(new GridLayout());
-		  final TableViewer viewer = new TableViewer(tableComposite,
-				  SWT.BORDER | SWT.FULL_SELECTION);
-		  TableLayout layout = new TableLayout();
-		  layout.addColumnData(new ColumnWeightData(33, true));
-		  layout.addColumnData(new ColumnWeightData(33, true));
-		  layout.addColumnData(new ColumnWeightData(33, true));
-		  viewer.getTable().setLayout(layout);
-		  viewer.getTable().setLinesVisible(true);
-		  viewer.getTable().setHeaderVisible(true);
 		  
-		  TableColumn column1 = new TableColumn(viewer.getTable(),SWT.CENTER);
-		  column1.setText("Name");
-		  column1.setWidth(400);
-		  TableColumn column2 = new TableColumn(viewer.getTable(), SWT.CENTER);
-		  column2.setText("Group");
-		  TableColumn column3 = new TableColumn(viewer.getTable(), SWT.CENTER);
-		  column3.setText("SWT done");
+		  StudentTable table = new StudentTable(tableComposite,SWT.BORDER | SWT.FULL_SELECTION);
 		  
+		  Student one = new Student();
+		  one.setName("Yaroslav");
+		  one.setGroup("1");
+		  one.setTaskDone(true);
+		  
+		  Student two = new Student();
+		  two.setName("Kristina");
+		  two.setGroup("7");
+		  two.setTaskDone(null);
+		  
+		  List <Student> students = new ArrayList <Student>();
+		  students.add(one);
+		  students.add(two);
+		  
+		  table.setInput(students);
+		 		  
 		  Composite studentComposite = new Composite(mainPane, SWT.NONE);
-		  studentComposite.setLayout(new GridLayout(4, false));
+		  studentComposite.setLayout(new GridLayout(3, false));
 		  studentComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		  Label nameLabel = new Label(studentComposite, SWT.LEFT);
 		  nameLabel.setText("Name");
 		  nameLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		  Text nameText = new Text(studentComposite, SWT.SINGLE | SWT.BORDER);
 		  GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		  gridData.horizontalSpan = 3;
+		  gridData.horizontalSpan = 2;
 		  nameText.setLayoutData(gridData);
 		  
 		  Label groupLabel = new Label(studentComposite, SWT.LEFT);
@@ -66,13 +67,13 @@ public class Main extends ApplicationWindow {
 		  groupLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		  Text groupText = new Text(studentComposite, SWT.SINGLE | SWT.BORDER);
 		  gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		  gridData.horizontalSpan = 3;
+		  gridData.horizontalSpan = 2;
 		  groupText.setLayoutData(gridData);
 		  
 		  Button taskDone = new Button(studentComposite, SWT.CHECK);
 		  taskDone.setText("SWT task done");
 		  gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		  gridData.horizontalSpan = 4;
+		  gridData.horizontalSpan = 3;
 		  taskDone.setLayoutData(gridData);
 		  
 		  Button addNew = new Button(studentComposite,SWT.NONE);
@@ -82,10 +83,6 @@ public class Main extends ApplicationWindow {
 		  Button save = new Button(studentComposite,SWT.NONE);
 		  save.setText("Save");
 		  save.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
-		  
-		  Button delete = new Button(studentComposite,SWT.NONE);
-		  delete.setText("Delete");
-		  delete.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
 		  
 		  Button cancel = new Button(studentComposite,SWT.NONE);
 		  cancel.setText("Cancel");
@@ -110,43 +107,34 @@ public class Main extends ApplicationWindow {
 			  }
 		  });
 		  
-		 
-		  
 		  MenuManager editMenu = new MenuManager("&Edit");
 		  getMenuBarManager().add(editMenu);
 		  editMenu.add(new Separator());
 		  editMenu.add(new Action("&New"){
-			  
 			  @Override
 			  public void run() {
 				  //TODO
 			  }
 		  });
-		  
-		  editMenu.add(new Action("&Save"){
-			  
+		  editMenu.add(new Action("&Save"){	  
 			  @Override
 			  public void run() {
 				  //TODO
 			  }
 		  });
-		  
-		  editMenu.add(new Action("&Delete"){
-			  
+		  editMenu.add(new Action("&Delete"){  
 			  @Override
 			  public void run() {
 				  //TODO
 			  }
 		  });
-		  
-		  editMenu.add(new Action("&Cancel"){
-			  
+		  editMenu.add(new Action("&Cancel"){ 
 			  @Override
 			  public void run() {
 				  //TODO
 			  }
 		  });
-		  
+
 		  MenuManager helpMenu = new MenuManager("&Help");
 		  getMenuBarManager().add(helpMenu);
 		  helpMenu.add(new Separator());
@@ -156,15 +144,13 @@ public class Main extends ApplicationWindow {
 				  //TODO
 			  }
 		  });  
-		  
-		  
 	  }
-	  
-	  public static void main(String[] args) {	  
-	    
+
+	  public static void main(String[] args) {
 		  Main awin = new Main();	    
 		  awin.setBlockOnOpen(true);	    
-		  awin.open();	    
+		  awin.open();
+
 		  Display.getCurrent().dispose();
 	  }
 	}
---
+++ lux.task.jface/src/main/java/lux/task/jface/StudentTable.java
@@ -0,0 +1,89 @@
+package lux.task.jface;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableEditor;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
+public class StudentTable extends TableViewer {
+	public StudentTable(Composite parent, int style){
+		super(parent, style);
+	
+		this.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	    this.getTable().setHeaderVisible(true);
+	    this.getTable().setLinesVisible(true);
+	    this.setContentProvider(new ArrayContentProvider());
+	
+	    TableViewerColumn nameCol = new TableViewerColumn(this, SWT.LEFT, 0);
+	    nameCol.getColumn().setText("Name");
+	    nameCol.setLabelProvider(new ColumnLabelProvider(){
+	        @Override
+	        public String getText(Object element) {
+	            Student student = (Student)element;
+	            return student.getName();
+	        }
+	    });
+	    
+	    TableViewerColumn groupCol = new TableViewerColumn(this, SWT.LEFT, 1);
+        groupCol.getColumn().setText("Group");
+        groupCol.setLabelProvider(new ColumnLabelProvider(){
+            @Override
+            public String getText(Object element) {
+            	Student student = (Student)element;
+                return student.getGroup();
+            }
+        });
+
+        TableViewerColumn doneCol = new TableViewerColumn(this, SWT.LEFT, 2);
+        doneCol.getColumn().setText("SWT done");
+        doneCol.getColumn().setWidth(100);
+        doneCol.setLabelProvider(new ColumnLabelProvider(){
+            @Override
+            public void update(ViewerCell cell) {
+                TableItem item = (TableItem) cell.getItem();
+                Button button = new Button((Composite) cell.getViewerRow().getControl(), SWT.CHECK);
+                TableEditor editor = new TableEditor(item.getParent());
+                editor.grabHorizontal  = true;
+                editor.grabVertical = true;
+                editor.setEditor(button , item, cell.getColumnIndex());
+                editor.layout();
+            }
+        });
+        
+        TableViewerColumn deleteCol = new TableViewerColumn(this, SWT.LEFT, 3);
+        deleteCol.getColumn().setWidth(100);
+        deleteCol.setLabelProvider(new ColumnLabelProvider(){
+            @Override
+            public void update(ViewerCell cell) {
+                TableItem item = (TableItem) cell.getItem();
+                Button button = new Button((Composite) cell.getViewerRow().getControl(), SWT.NONE);
+                button.setText("Delete");
+                TableEditor editor = new TableEditor(item.getParent());
+                editor.grabHorizontal  = true;
+                editor.grabVertical = true;
+                editor.setEditor(button , item, cell.getColumnIndex());
+                editor.layout();
+            }
+        });
+                
+        this.getTable().addListener(SWT.Resize, new Listener() {
+        	
+			public void handleEvent(final Event event) {
+				for (TableColumn c : getTable().getColumns()) {
+					c.pack();
+				}
+				
+			}        	
+        });        	
+	}
+}
